f93631d5ab6b47572d7345b339658f5542b32ed5
Mark Debbane <markdebbane@gmail.com>
2020-09-11 13:08:30 +0300

    Chnaged DB

--- api/tag/models/tag.js
+++ api/tag/models/tag.js
@@ -4,5 +4,26 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
+const slugify = require('slugify');
+const category = require('../../category/services/category');
+module.exports = {
+    lifecycles: {
+        async beforeCreate(data) {
+            console.log("BeforeCreate", data);
+            data.slug = slugify(data.title, { lower: true });
 
-module.exports = {};
+        },
+        async beforeUpdate(params, data) {
+            console.log("BeforeUpdate", data);
+            data.slug = slugify(data.title, { lower: true });
+
+        },
+        async afterCreate(data) {
+            console.log('AfterCreate', data);
+
+        },
+        async afterUpdate(data) {
+            console.log('AfterUpdate', data);
+        }
+    },
+};
--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@
 //       connector: 'bookshelf',
 //       settings: {
 //         client: 'postgres',
-//         host: env('DATABASE_HOST', 'http://139.59.145.204'),
+//         host: env('DATABASE_HOST', '127.0.0.1'),
 //         port: env.int('DATABASE_PORT', 5432),
 //         database: env('DATABASE_NAME', 'fawra'),
 //         username: env('DATABASE_USERNAME', 'postgres'),
@@ -18,6 +18,8 @@
 //   },
 // });
 
+
+//FOR LOCAL USE
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
