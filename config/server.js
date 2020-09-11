902c21a6ad45f971dc0b2f2009aa8adce02931fb
Mark Debbane <markdebbane@gmail.com>
2020-09-11 14:36:44 +0300

    Updated Tags

--- config/server.js
+++ config/server.js
@@ -2,7 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
-    url: '/dashboard', // We change the path to access to the admin (highly recommended for security reasons).
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'ed87eec228aa9d159b29c5d9bbdde7e0'),
     },
